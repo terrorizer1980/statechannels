036d7e87fec0be4a6748453d7eaa7fa6a881cb36
George Knee <georgeknee@googlemail.com>
2020-01-23 16:06:25 +0000

    Entire machine composed of typed StateNodeConfigs

--- packages/rps/src/xstate/xstate.ts
+++ packages/rps/src/xstate/xstate.ts
@@ -1,7 +1,7 @@
 import {createMachine, StateNodeConfig} from 'xstate';
-// import {GameAction} from 'src/redux/game/actions';
-// import {GlobalAction} from 'src/redux/global/actions';
-// import {AnyAction} from 'src/redux/login/actions';
+import {GameAction} from 'src/redux/game/actions';
+import {GlobalAction} from 'src/redux/global/actions';
+import {AnyAction} from 'src/redux/login/actions';
 import {MetamaskAction} from 'src/redux/metamask/actions';
 
 type TContext = any;
@@ -11,131 +11,185 @@ interface TState {
 }
 type TEvent = any;
 
-// // game machine
-// const PlayerAStates = createMachine<TContext, GameAction, TState>({
-//   initial: 'GameChosen',
-//   states: {
-//     GameChosen: {on: {StartRound: 'ChooseWeapon'}},
-//     ChooseWeapon: {on: {ChooseWeapon: 'WeaponChosen'}},
-//     WeaponChosen: {on: {ChooseSalt: 'WeaponAndSaltChosen'}},
-//     WeaponAndSaltChosen: {
-//       on: {
-//         ResultArrived: [
-//           {target: 'ResultPlayAgain', cond: 'sufficientFunds'},
-//           {target: 'InsufficientFunds', cond: 'insufficientFunds'},
-//         ],
-//       },
-//     },
-//     ResultPlayAgain: {on: {PlayAgain: 'WaitForRestart'}},
-//     WaitForRestart: {on: {StartRound: 'ChooseWeapon'}},
-//     InsufficientFunds: {},
-//     Resigned: {},
-//   },
-// });
+// game machine
+interface PlayerASchema {
+  states: {
+    GameChosen: any;
+    ChooseWeapon: any;
+    WeaponChosen: any;
+    WeaponAndSaltChosen: any;
+    ResultPlayAgain: any;
+    WaitForRestart: any;
+    InsufficientFunds: any;
+    Resigned: any;
+  };
+}
+const PlayerAStates: StateNodeConfig<TContext, PlayerASchema, GameAction> = {
+  initial: 'GameChosen',
+  states: {
+    GameChosen: {on: {StartRound: 'ChooseWeapon'}},
+    ChooseWeapon: {on: {ChooseWeapon: 'WeaponChosen'}},
+    WeaponChosen: {on: {ChooseSalt: 'WeaponAndSaltChosen'}},
+    WeaponAndSaltChosen: {
+      on: {
+        ResultArrived: [
+          {target: 'ResultPlayAgain', cond: 'sufficientFunds'},
+          {target: 'InsufficientFunds', cond: 'insufficientFunds'},
+        ],
+      },
+    },
+    ResultPlayAgain: {on: {PlayAgain: 'WaitForRestart'}},
+    WaitForRestart: {on: {StartRound: 'ChooseWeapon'}},
+    InsufficientFunds: {},
+    Resigned: {},
+  },
+};
 
-// const PlayerBStates = createMachine<TContext, GameAction, TState>({
-//   initial: 'CreatingOpenGame',
-//   states: {
-//     CreatingOpenGame: {on: {CreateGame: 'WaitingRoom'}},
-//     WaitingRoom: {on: {GameJoined: 'OpponentJoined'}},
-//     OpponentJoined: {on: {StartRound: 'ChooseWeapon'}},
-//     ChooseWeapon: {on: {ChooseWeapon: 'WeaponChosen'}},
-//     WeaponChosen: {
-//       on: {
-//         ResultArrived: [
-//           {target: 'ResultPlayAgain', cond: 'sufficientFunds'},
-//           {target: 'InsufficientFunds', cond: 'insufficientFunds'},
-//         ],
-//       },
-//     },
-//     ResultPlayAgain: {on: {PlayAgain: 'WaitForRestart'}},
-//     WaitForRestart: {on: {StartRound: 'ChooseWeapon'}},
-//     InsufficientFunds: {},
-//     Resigned: {},
-//   },
-// });
+interface PlayerBSchema {
+  states: {
+    CreatingOpenGame: any;
+    WaitingRoom: any;
+    OpponentJoined: any;
+    ChooseWeapon: any;
+    WeaponChosen: any;
+    ResultPlayAgain: any;
+    WaitForRestart: any;
+    InsufficientFunds: any;
+    Resigned: any;
+  };
+}
+const PlayerBStates: StateNodeConfig<TContext, PlayerBSchema, GameAction> = {
+  initial: 'CreatingOpenGame',
+  states: {
+    CreatingOpenGame: {on: {CreateGame: 'WaitingRoom'}},
+    WaitingRoom: {on: {GameJoined: 'OpponentJoined'}},
+    OpponentJoined: {on: {StartRound: 'ChooseWeapon'}},
+    ChooseWeapon: {on: {ChooseWeapon: 'WeaponChosen'}},
+    WeaponChosen: {
+      on: {
+        ResultArrived: [
+          {target: 'ResultPlayAgain', cond: 'sufficientFunds'},
+          {target: 'InsufficientFunds', cond: 'insufficientFunds'},
+        ],
+      },
+    },
+    ResultPlayAgain: {on: {PlayAgain: 'WaitForRestart'}},
+    WaitForRestart: {on: {StartRound: 'ChooseWeapon'}},
+    InsufficientFunds: {},
+    Resigned: {},
+  },
+};
 
-// const game = createMachine<TContext, GameAction, TState>({
-//   initial: 'Empty',
-//   states: {
-//     Empty: {
-//       on: {
-//         UpdateProfile: 'NeedAddress',
-//       },
-//     },
-//     NeedAddress: {
-//       on: {
-//         GotAddressFromWallet: 'Lobby',
-//       },
-//     },
-//     Lobby: {
-//       on: {
-//         JoinOpenGame: 'PlayerA',
-//         CreateGame: 'PlayerB',
-//       },
-//     },
-//     PlayerA: {
-//       on: {GameOver: 'GameOver'},
-//       ...PlayerAStates.states,
-//     },
-//     PlayerB: {
-//       on: {GameOver: 'GameOver'},
-//       ...PlayerBStates.states,
-//     },
-//     GameOver: {on: {ExitToLobby: 'Lobby'}},
-//   },
-// });
+interface GameSchema {
+  states: {
+    Empty: any;
+    NeedAddress: any;
+    Lobby: any;
+    PlayerA: PlayerASchema;
+    PlayerB: PlayerBSchema;
+    GameOver: any;
+  };
+}
+const game: StateNodeConfig<TContext, GameSchema, GameAction> = {
+  initial: 'Empty',
+  states: {
+    Empty: {
+      on: {
+        UpdateProfile: 'NeedAddress',
+      },
+    },
+    NeedAddress: {
+      on: {
+        GotAddressFromWallet: 'Lobby',
+      },
+    },
+    Lobby: {
+      on: {
+        JoinOpenGame: 'PlayerA',
+        CreateGame: 'PlayerB',
+      },
+    },
+    PlayerA: {
+      on: {GameOver: 'GameOver'},
+      ...PlayerAStates,
+    },
+    PlayerB: {
+      on: {GameOver: 'GameOver'},
+      ...PlayerBStates,
+    },
+    GameOver: {on: {ExitToLobby: 'Lobby'}},
+  },
+};
 
-// // global machine
-// const rules = createMachine<TContext, GlobalAction, TState>({
-//   initial: 'invisible',
-//   states: {
-//     visible: {on: {'APP.TOGGLE_RULES_VISIBILITY': 'invisible'}},
-//     invisible: {on: {'APP.TOGGLE_RULES_VISIBILITY': 'visible'}},
-//   },
-// });
-// const wallet = createMachine<TContext, GlobalAction, TState>({
-//   initial: 'invisible',
-//   states: {
-//     visible: {on: {'APP.HIDE_WALLET': 'invisible'}},
-//     invisible: {on: {'APP.SHOW_WALLET': 'visible'}},
-//   },
-// });
-// const globall = createMachine<TContext, GlobalAction, TState>({
-//   states: {
-//     rules: rules.states,
-//     wallet: wallet.states,
-//   },
-// });
+// global machine
+interface GlobalSchema {
+  states: {
+    rules: VisibilitySchema;
+    wallet: VisibilitySchema;
+  };
+}
 
-// // login machine
-// const loading = createMachine<TContext, AnyAction, TState>({
-//   initial: 'false',
-//   states: {
-//     true: {on: {'LOGIN.SUCCESS': 'false'}},
-//     false: {on: {'LOGIN.REQUEST': 'true'}},
-//   },
-// });
-// const loggedIn = createMachine<TContext, AnyAction, TState>({
-//   initial: 'false',
-//   states: {
-//     true: {},
-//     false: {on: {'LOGIN.SUCCESS': 'true'}},
-//   },
-// });
-// const login = createMachine<TContext, AnyAction, TState>({
-//   states: {
-//     loading: loading.states,
-//     loggedIn: loggedIn.states,
-//   },
-// });
+interface VisibilitySchema {
+  states: {
+    visible: any;
+    invisible: any;
+  };
+}
+const rules: StateNodeConfig<TContext, VisibilitySchema, GlobalAction> = {
+  initial: 'invisible',
+  states: {
+    visible: {on: {'APP.TOGGLE_RULES_VISIBILITY': 'invisible'}},
+    invisible: {on: {'APP.TOGGLE_RULES_VISIBILITY': 'visible'}},
+  },
+};
+const wallet: StateNodeConfig<TContext, VisibilitySchema, GlobalAction> = {
+  initial: 'invisible',
+  states: {
+    visible: {on: {'APP.HIDE_WALLET': 'invisible'}},
+    invisible: {on: {'APP.SHOW_WALLET': 'visible'}},
+  },
+};
+const global: StateNodeConfig<TContext, GlobalSchema, GlobalAction> = {
+  states: {
+    rules,
+    wallet,
+  },
+};
+
+// login machine
+interface LoadingSchema {
+  states: {
+    loading: any;
+    loggedIn: any;
+  };
+}
+const loading: StateNodeConfig<TContext, BooleanSchema, AnyAction> = {
+  initial: 'false',
+  states: {
+    true: {on: {'LOGIN.SUCCESS': 'false'}},
+    false: {on: {'LOGIN.REQUEST': 'true'}},
+  },
+};
+const loggedIn: StateNodeConfig<TContext, BooleanSchema, AnyAction> = {
+  initial: 'false',
+  states: {
+    true: {},
+    false: {on: {'LOGIN.SUCCESS': 'true'}},
+  },
+};
+const login: StateNodeConfig<TContext, LoadingSchema, AnyAction> = {
+  states: {
+    loading,
+    loggedIn,
+  },
+};
 
 // metamask machine
 
 interface BooleanSchema {
   states: {
-    true: {};
-    false: {};
+    true: any;
+    false: any;
   };
 }
 
@@ -174,9 +228,9 @@ export const rps = createMachine<TContext, TEvent, TState>({
   id: 'rps',
   type: 'parallel',
   states: {
-    // game: game.states,
-    // global: globall.states,
-    // login: login.states,
+    game,
+    global,
+    login,
     metamask,
   },
 });
