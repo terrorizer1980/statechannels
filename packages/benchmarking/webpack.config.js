d27342fcdd523468779f4cb085efa2b9f1c9ae56
George Knee <georgeknee@googlemail.com>
2020-06-29 14:32:15 +0100

    Modify webpack config to deal with node modules (temp)

---
+++ packages/benchmarking/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    fs: 'empty',
+    child_process: 'empty'
+  }
+};
